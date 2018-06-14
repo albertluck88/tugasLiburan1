776d0416a860d87f476b6867c5d3d66784080ea1
albertluck88 <39264172+albertluck88@users.noreply.github.com>
2018-06-14 17:44:25 +0700

    ubah salah kirim

--- Tugas 09 Juni/tugas01.js
+++ Tugas 09 Juni/tugas01.js
@@ -1,54 +1,53 @@
 function soal1(param){
 
-    var result = []
-  
-    var hasil = 0    
-    for(var i=0;i<=param;i++)
-    {
-      hasil = hasil + i
-    }
-  
-    if(param % 2 === 0 && param !== 0){
-      for(var j = 1; j <= param; j++){
-        if(j === hasil % param + 1 || j === hasil % param)
-        {
-          result.push("*")
-        }else{
-          result.push(" ")
-        }
-      }
-  
-      }else if(param === 0 ){
-        return "invalid input"
-  
-      }else{
-        for(var k=1;k<=param;k++){
-          if(k === Math.floor(hasil / param)){
-            result.push("*")
-          }else{
-            result.push(" ")
-          }
-        }
-      }
-  return result
-    }
+  var result = []
+  var hasil = 0
+  for(var i=1; i<=param; i++){
+   
+  if(param % 2 === 0 && param !== 0){
     
+      if(i === param / 2 + 1 || i === param / 2)
+      {
+        result.push("*")
+      }else{
+        result.push(" ")
+      }
+    }else{
+
+      if(i === Math.round(param / 2)){
+        result.push("*")
+      }else{
+        result.push(" ")
+      }
+
+     
+     
+    }
+
+
+  }
+if (param !== 0){
+  return result
+}else{
+  return "INVALID INPUT"
+}
+}
   
-  
-  // test case
-  console.log( soal1(5) ) 
-  /*
-      output : 
-      ['','','*','','']
-  */
-  
-  console.log( soal1(4)) // ['','*','*','']
-  
-  console.log( soal1(7)) // ['','','','*','','','']
-  
-  console.log( soal1(10)) // ['','','','','*','*','','','','']
-  
-  console.log( soal1(0)) // invalid input 
-  
-  console.log( soal1(1)) // ['*']
-  
+
+
+console.log( soal1(5) ) 
+/*
+    output : 
+    ['','','*','','']
+*/
+
+console.log( soal1(4)) // ['','*','*','']
+
+console.log( soal1(7)) // ['','','','*','','','']
+
+console.log( soal1(10)) // ['','','','','*','*','','','','']
+
+console.log( soal1(0)) // invalid input 
+
+console.log( soal1(1)) // ['*']
+
